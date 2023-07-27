697a563dc903647c364f9a62e01f8d0536ecd91f
ruannysil <ruannysilva67@gmail.com>
2023-07-27 15:57:26 -0300

    feat: add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#590bd8',
+        primaryDarker: '#312a4f',
+        primaryLighter: '#ddd6ea',
+        grayPrimary: '#717171',
+        grayLighter: '#bbbfbf',
+      },
+      textColor: {
+        dark: '#717171',
+      },
     },
   },
   plugins: [],
-}
+};
