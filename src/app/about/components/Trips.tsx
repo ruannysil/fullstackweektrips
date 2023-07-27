6e7cadcacce9480a78b8dfad95cf4617803366e8
ruannysil <ruannysilva67@gmail.com>
2023-07-27 10:10:22 -0300

    feat: update database schema

--- src/app/about/components/Trips.tsx
+++ src/app/about/components/Trips.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+// 'use client';
 
-const Trips = () => {
+import { prisma } from '@/lib/prisma';
+import React from 'react';
+
+const getTrip = async () => {
+  const trips = await prisma.trip.findMany({});
+
+  return trips;
+};
+
+const Trips = async () => {
+  const data = await fetch('http://jsonplaceholder.typicode.com/posts').then((res) => res.json());
+
   return (
     <div>
-      Trips
+      {data.map((i: any) => (
+        <p key={i.id}>{i.title}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Trips
+export default Trips;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Trips from './components/Trips'
 
+
+export const metadata = {
+  title: "About"
+}
+
 const Page = () => {
   return (
     <div>
