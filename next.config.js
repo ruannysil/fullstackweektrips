3596f519639c8dc6063a08bf176c3f4c8220e9a9
ruannysil <ruannysilva67@gmail.com>
2023-07-28 11:57:48 -0300

    feat:  add trip item component

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-   images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import QuickSearch from "./components/QuickSearch";
-import TripSearch from "./components/TripSearch";
+import QuickSearch from './components/QuickSearch';
+import RecommendedTrips from './components/RecommendedTrips';
+import TripSearch from './components/TripSearch';
 
 export default function Home() {
-
-
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
