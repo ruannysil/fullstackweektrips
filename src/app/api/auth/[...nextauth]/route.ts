f19ea47c5d8de573a4b340e746c24e762ad95feb
ruannysil <ruannysilva67@gmail.com>
2023-07-30 10:06:50 -0300

    feat: add trip buy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,8 +12,19 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
 
-export  {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import Header from '../components/Header';
 import Footer from '@/components/Footer';
+import ToastProvider from '@/providers/toast';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -24,10 +25,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
+          <ToastProvider>
           <Header />
           {children}
           <Footer />
-          </NextAuthProvider>
+          </ToastProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
