ec93f59301fda88527da27c60eedac202391f83d
ruannysil <ruannysilva67@gmail.com>
2023-07-31 10:38:30 -0300

    feat: add pege reload on trip cancelation

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -13,20 +13,20 @@ const MyTrips = () => {
   const { status, data } = useSession();
 
   const router = useRouter();
+  
+  const fetchReservations = async () => {
+    const response = await fetch(
+      `http://localhost:3000/api/user/${(data?.user as any)?.id}/reservaitons`,
+    );
+    const json = await response.json();
 
+    setReservations(json);
+  };
   useEffect(() => {
-    if (status === 'unauthenticated' || !data?.user) {
+    if (status === 'unauthenticated') {
       return router.push('/');
     }
 
-    const fetchReservations = async () => {
-      const response = await fetch(
-        `http://localhost:3000/api/user/${(data?.user as any).id}/reservaitons`,
-      );
-      const json = await response.json();
-
-      setReservations(json);
-    };
 
     fetchReservations();
   }, [status]);
