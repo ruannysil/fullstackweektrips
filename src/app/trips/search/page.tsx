a62a3daf8e28de23ef2ed1cf52b7e48f336cee88
ruannysil <ruannysilva67@gmail.com>
2023-08-02 13:35:12 -0300

    fix: update code and erro

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -1,33 +1,38 @@
-'use client'
+"use client";
 
-import { useSearchParams } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
-import { Trip } from '@prisma/client';
-import TripItem from '@/components/TripItem';
+import TripItem from "@/components/TripItem";
+import { Trip } from "@prisma/client";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 const Trips = () => {
-  const [trips, setTrips] = useState<Trip[]>([]);
+  const [trips, setTrips] = React.useState<Trip[]>([]);
+
   const searchParams = useSearchParams();
 
   useEffect(() => {
     const fetchTrips = async () => {
       const response = await fetch(
-        `/api/trips/search?text=${searchParams.get("text") ?? ""}&startDate=${searchParams.get("startDate")}&budget=${searchParams.get("budget")}`,
+        `/api/trips/search?text=${searchParams.get("text") ?? ""}&startDate=${searchParams.get("startDate")}&budget=${searchParams.get("budget")}`
       );
+
       const data = await response.json();
+
       setTrips(data);
     };
+
     fetchTrips();
-  }, [searchParams]);
+  }, []);
 
   return (
-    <div className='container flex flex-col items-center p-5 mx-auto'>
-      <h1 className='text-xl font-semibold text-primaryDarker'>Viagens Encontradas</h1>
-      <h2 className='mb-5 font-medium text-grayPrimary'>
-        {trips.length > 0 ? "Listamos as melhores hóspedes para você!" : "Não encontramos nada com os dados fornecidos!"}
+    <div className="container flex flex-col items-center p-5 mx-auto lg:items-start lg:pt-10">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">Viagens Encontradas</h1>
+      <h2 className="mb-5 font-medium text-grayPrimary lg:mt-6 lg:w-full lg:text-left">
+        {trips.length > 0 ? "Listamos as melhores viagens pra você!" : "Não encontramos nada nos seus parâmetros! =("}
       </h2>
-      <div className='flex flex-col gap-4'>
-        {trips?.map(trip => (
+
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
+        {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
       </div>
